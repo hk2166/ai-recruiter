3195ffaca68e87f324e3b3c8bf39531b6d0726db
Hemant <133942800+hk2166@users.noreply.github.com>
2025-10-06 13:06:23 +0530

    made the .en.example for issue23

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANONKEY
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 export const supabase = createClient(
     supabaseUrl,
     supabaseKey
